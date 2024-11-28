3b38ce692d13cccb37131597181e4266bcef9377
mrtnmmn <mrtn.lmba@gmail.com>
2024-11-29 00:49:31 +0100

    deleted console log

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,7 +4,6 @@ const express = require('express');
 
 const router = express.Router()
 
-console.log('here')
 router.use('/auth', authRoutes);
 router.use('/', (_req: any, res: { send: (arg0: string) => any; }) => res.send('Hello, TypeScript with Node.js!'));
 
